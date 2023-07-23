fd14dbb443d095ef20edab3b3d740d586b5f3fb1
rtygya <rxg3756@mavs.uta.edu>
2023-07-23 18:53:53 -0500

    added commit, abort, wait_die functions

--- project1.py
+++ project1.py
@@ -2,7 +2,7 @@
 # Rakshav Patel – 10011941754
 
 # Get input file and store it in a variable
-input_file = "Input 4.txt"
+input_file = "Input 2.txt"
 # input_file = input("Enter input file name: ")
 
 output_file = "Output " + input_file[6] + ".txt"
@@ -53,13 +53,32 @@ class Transaction:
         self.blocked_operations = []    # if transaction is blocked, further operations from input file go here to be resumed later
         
     def lock_item(self, item):
-        if item not in self.locked_items:
-            self.locked_items.append(item)
+        self.locked_items.append(item)
             
     def add_blocked_op(self, op):
-        if op not in self.locked_items:
-            self.blocked_operations.append(op)
-            
+        self.blocked_operations.append(op)
+                    
+    def commit(self): 
+        for i in self.locked_items:     # unlock all items locked by this transaction
+            locTableItem.unlock(i)      
+        self.state = C  # state is now committed
+                
+    def abort(self):
+        for i in self.locked_items:     # unlock all items locked by this transaction
+            locTableItem.unlock(i)      
+        self.state = "aborted"  # state is now aborted
+        
+    def block(self):
+        self.state = "blocked"
+        
+# Wait-die function
+def wait_die(t1, t2):
+    # t1 currently requests an item locked by t2
+    if t1.ts < t2.ts:
+        t1.block()  # block if t1 older
+    else:
+        t1.abort()  # abort if t1 younger
+    
 # Function to easily find transaction with specific id
 def find_transaction(target_id):
     for t in transTableItem:
@@ -77,26 +96,41 @@ try:
             # Store transaction ID and operation in variables
             t_id = line[1]
             op = line[0]
-
-            if (op == 'b'):
+            transaction = find_transaction(t_id)
+            
+            if transaction:
+                if not transaction.state == "aborted":  # Process operations only if transaction is not aborted
+                    if transaction.state == "blocked":
+                        # Blocked transaction, add operation to waiting list      
+                        transaction.add_blocked_op(op)  
+                    elif (op == 'r'):
+                        # Transaction readlocks item 
+                        item = line[3]
+                        modified_line = line.strip() + ' T' + t_id + ' reads item ' + item  + '\n'
+                    elif (op == 'w'):
+                        # Transaction writelocks item 
+                        item = line[3]
+                        modified_line = line.strip() + ' T' + t_id + ' writes item ' + item  + '\n'
+                    elif (op == 'e'):
+                        # Commit transaction 
+                        transaction.commit()
+                        modified_line = line.strip() + ' T' + t_id + ' is ' + transaction.state + '\n'
+            elif (op == 'b'):
                 # Begin transaction
                 transTableItem.append(Transaction(t_id, timestamp, A)) 
                 modified_line = line.strip() + ' T' + t_id + ' begins. TS=' + str(timestamp) + '. Id=' + t_id + '. state=' + A + '.\n'
-                timestamp += 1  
-            elif (op == 'e'):
-                # Commit transaction 
-                modified_line = line.strip() + ' T' + t_id + ' is committed.\n'
-            elif (op == 'r'):
-                # Transaction readlocks item 
-                item = line[3]
-                modified_line = line.strip() + ' T' + t_id + ' reads item ' + item  + '\n'
-            elif (op == 'w'):
-                # Transaction writelocks item 
-                item = line[3]
-                modified_line = line.strip() + ' T' + t_id + ' writes item ' + item  + '\n'
+                timestamp += 1
             
             output.write(modified_line)
-
+        
+        # Write the final states of each transaction at the very end
+        final_line = "Final state:\n"
+        output.write(final_line)
+        for tr in transTableItem:
+            final_line = 'T' + tr.id + ': ' + tr.state + '. '
+            output.write(final_line)
+        output.write('\n')
+        
     print("Output file created:", output_file)
 except FileNotFoundError:
         print("Error: Input file not found.")
