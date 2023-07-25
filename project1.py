49c012e6594b57110d91d76721f53a312240e098
rtygya <rxg3756@mavs.uta.edu>
2023-07-25 14:44:28 -0500

    added write_file()

--- project1.py
+++ project1.py
@@ -5,10 +5,17 @@ import copy
 import os
 
 # Get input file and store it in a variable
-input_file = "Input 2.txt"
+input_file = "Input 1.txt"
 # input_file = input("Enter input file name: ")
 
 output_file = "Output " + input_file[6] + ".txt"
+
+# Function to write text to output file
+def write_file(file, text):
+    with open(file, 'a') as output:
+        if text:
+            output.write(text)
+        
 
 #array that will hold diffrent state of transaction
 input_operation = []
@@ -55,23 +62,27 @@ def readlock(i, X):
     item = find_lock_item(X)
     if not item:
         locTableItem.append(lock_table("read-locked", X, i))
-        print(locTableItem[-1].lock_item + ' is read-locked by' + ' T' + str(i) + '. ') 
+        modified_line = ' ' + locTableItem[-1].lock_item + ' is read-locked by' + ' T' + str(i) + '. \n'
+        write_file(output_file, modified_line)
         return 1
     elif item.lock_state == "read-locked" and i in item.lockHoldBy:
-        print(' T' + i + ' already has write lock on ' + X)
+        modified_line = ' T' + i + ' already has write lock on ' + X + '. \n'
+        write_file(output_file, modified_line)
         return 1
     elif item.lock_state == "read-locked" and i not in item.lockHoldBy:
         item.add_locking_trxn(i)
-        print(item.lock_item + ' is read-locked by' + ' T' + str(i) + '. ') 
+        modified_line = ' ' + item.lock_item + ' is read-locked by' + ' T' + str(i) + '. \n'
+        write_file(output_file, modified_line)
         return 1
     elif item.lock_state == "write-locked" and item.lockHoldBy[0] == i:
         item.lockHoldBy.clear()
         item.add_locking_trxn(i)
         item.ChangeLock_State("read-locked")
-        print(item.lock_item + ' is read-locked by' + ' T' + str(i) + '. ') 
+        modified_line = ' ' + item.lock_item + ' is read-locked by' + ' T' + str(i) + '. \n'
+        write_file(output_file, modified_line)
         return 1
     elif item.lock_state == "write-locked" and item.lockHoldBy[0] != i:  
-        print(find_transaction(i).wait_die(find_transaction(item.lockHoldBy[0]), item))
+        find_transaction(i).wait_die(find_transaction(item.lockHoldBy[0]), item)
         return 0
         
 # Writelock function
@@ -79,21 +90,24 @@ def writelock(i, X):
     item = find_lock_item(X)
     if not item:
         locTableItem.append(lock_table("write-locked", X, i))
-        print(' T' + i + ' write-locked. ') 
+        modified_line = ' T' + i + ' write-locked. \n'
+        write_file(output_file, modified_line) 
         return 1
     elif item.lock_state == "write-locked" and item.lockHoldBy[0] == i:
-        print(' T' + i + ' already has write lock on ' + X)
+        modified_line = ' T' + i + ' already has write lock on ' + X + "\n"
+        write_file(output_file, modified_line) 
         return 1
     elif item.lock_state == "read-locked" and len(item.lockHoldBy) == 1 and item.lockHoldBy[0] == i:
         item.lockHoldBy.clear()
         item.add_locking_trxn(i)
         item.ChangeLock_State("write-locked")
-        print(' T' + i + ' upgraded from read-locked to write-locked') 
+        modified_line = ' read lock on ' + item.lock_item + ' by T' + i + ' is upgraded to write lock. \n'
+        write_file(output_file, modified_line) 
         return 1
     elif item.lock_state == "read-locked" and len(item.lockHoldBy) >= 1:
         for tid in item.lockHoldBy:
             if tid != i:
-                print(find_transaction(i).wait_die(find_transaction(tid), item))
+                find_transaction(i).wait_die(find_transaction(tid), item)
     elif item.lock_state == "write-locked" and item.lockHoldBy[0] != i:
         find_transaction(i).wait_die(find_transaction(item.lockHoldBy[-1]), item)
         return 0
@@ -112,7 +126,8 @@ def unlock(item):
 def resume(transaction):
         # process all blocked operations
         for op in transaction.blocked_operations:
-            print(op + ' T' + op[1] + ' is released from operation list. ' + process_operation(op, timestamp) + '\n')
+            write_file(output_file, op + ' T' + op[1] + ' is released from operation list.')
+            process_operation(op, timestamp)
             
 # Transaction class
 class Transaction:
@@ -130,18 +145,14 @@ class Transaction:
         self.blocked_operations.append(op)
         if (op[0] == 'r'):
             item = op[3]
-            print(' T' + self.id + ' reading ' + item + ' is added to operation list.')
-            return ' T' + self.id + ' reading ' + item + ' is added to operation list. \n'
+            write_file(output_file, ' T' + self.id + ' reading ' + item + ' is added to operation list. \n')
         elif (op[0] == 'w'):
             item = op[3]
-            print(' T' + self.id + ' writing ' + item + ' is added to operation list.')
-            return ' T' + self.id + ' writing ' + item + ' is added to operation list. \n'
+            write_file(output_file, ' T' + self.id + ' writing ' + item + ' is added to operation list. \n')
         elif (op[0] == 'e'):
-            print('Committing T' + self.id + ' is added to operation list. ')
-            return 'Committing T' + self.id + ' is added to operation list. \n'
+            write_file(output_file, ' Committing T' + self.id + ' is added to operation list. \n')
         else:
-            print('Operation added to list. ')
-            return 'Operation added to list. \n'
+            write_file(output_file, 'Operation added to list. ')
     
     def read(self, item):
         if readlock(self.id, item):
@@ -157,8 +168,8 @@ class Transaction:
         for item in self.locked_items:
             unlock(item)    
         self.state = C  # state is now committed
-        print(' T' + self.id + ' is ' + self.state + '.')
-        return ' T' + self.id + ' is ' + self.state + '. \n'
+        modified_line = ' T' + self.id + ' is ' + self.state + '. \n'
+        write_file(output_file, modified_line)
         
                 
     def abort(self):
@@ -169,20 +180,21 @@ class Transaction:
     def wait_die(self, t2, item):
         # this transaction currently requests an item locked by t2
         if self.ts < t2.ts:
-            print('T' + self.id + ' is blocked/waiting due to wait-die')
+            modified_line = ' T' + self.id + ' is blocked/waiting due to wait-die. \n'
+            write_file(output_file, modified_line)
             self.state = "blocked"  # block if older
             item.waiting_transaction(find_transaction(self.id))
         else:
-            print('T' + self.id + ' is aborted due to wait-die')
+            modified_line = ' T' + self.id + ' is aborted due to wait-die. \n'
+            write_file(output_file, modified_line)
             self.abort()            # abort if younger
             
         
 # Begin function
 def begin(ts, t_id):
     transTableItem.append(Transaction(t_id, ts, A)) 
-    print(' T' + t_id + ' begins. Id=' + transTableItem[-1].id + ' TS=' + str(transTableItem[-1].ts) + '. state=' + A + '.')
-    return ' T' + t_id + ' begins. Id=' + transTableItem[-1].id + ' TS=' + str(transTableItem[-1].ts) + '. state=' + A + '.\n'
-        
+    modified_line = ' T' + t_id + ' begins. Id=' + transTableItem[-1].id + '. TS=' + str(transTableItem[-1].ts) + '. state=' + A + '. \n'
+    write_file(output_file, modified_line)        
 
     
 # Function to easily find transaction with specific id
@@ -210,61 +222,61 @@ def process_operation(line, timestamp):
     if transaction:
         if transaction.state == "aborted":  # If transaction is not aborted
             modified_line = ' T' + t_id + ' is already aborted. \n'
-            print(' T' + t_id + " is already aborted. ")
+            write_file(output_file, modified_line)
         else:
             if (op == 'r'):
             # Transaction reads item 
                 item = line[3]
                 if transaction.state == "blocked":
                     # Blocked transaction, add operation to waiting list      
-                    modified_line = transaction.add_blocked_op(line.strip())
+                    transaction.add_blocked_op(line.strip())
                 else:                    
                     # aquire a read lock for this item by this transaction
                     transaction.read(item)
-                    modified_line = ' T' + t_id + ' reads item ' + item  + '\n'
+                    #modified_line = ' T' + t_id + ' reads item ' + item  + '\n'
             
             elif (op == 'w'):
             # Transaction writes item 
                 item = line[3]
                 if transaction.state == "blocked":
                     # Blocked transaction, add operation to waiting list      
-                    modified_line = transaction.add_blocked_op(line.strip())
+                    transaction.add_blocked_op(line.strip())
                 else:                    
                     # aquire a write lock for this item by this transaction
                     transaction.write(item)
-                    modified_line = ' T' + t_id + ' writes item ' + item  + '\n'  
+                    #modified_line = ' T' + t_id + ' writes item ' + item  + '\n'  
             
             elif (op == 'e'):
             # Commit transaction 
                 if transaction.state == "blocked":
                     # Blocked transaction, add operation to waiting list      
-                    modified_line = transaction.add_blocked_op(line.strip())
+                    transaction.add_blocked_op(line.strip())
                 else:           
-                    modified_line = transaction.commit()        
+                    transaction.commit()        
     elif (line[0] == 'b'):    
         # Begin new transaction
-        modified_line = begin(timestamp, t_id)    
-    return modified_line
+        begin(timestamp, t_id)    
 
 # Reading and parsing each line in file 
 try:
     with open(input_file, 'r') as input:
         lines = input.readlines()
-
-    with open(output_file, 'w') as output:
-        # Process b, r, w, or e
-        for line in lines:           
-            output.write(line.strip() + process_operation(line.strip(), timestamp))
-            if line[0] == 'b':
-                timestamp += 1
         
-        # Write the final states of each transaction at the very end
-        final_line = "Final state:\n"
-        output.write(final_line)
-        for tr in transTableItem:
-            final_line = 'T' + tr.id + ': ' + tr.state + '. '
-            output.write(final_line)
-        output.write('\n')
+    open(output_file, "w")
+            
+    for line in lines:
+        write_file(output_file, line.strip())
+        process_operation(line.strip(), timestamp)
+        if line[0] == 'b':
+            timestamp += 1
+        
+    # Write the final states of each transaction at the very end
+    final_line = "\nFinal state:\n"
+    write_file(output_file, final_line)
+    for tr in transTableItem:
+        final_line = 'T' + tr.id + ': ' + tr.state + '. '
+        write_file(output_file, final_line)
+    write_file(output_file, '\n')
         
     print("Output file created:", output_file)
 except FileNotFoundError:
